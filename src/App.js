67f33600fbf911820fd3a0414007f49d597b0589
Stephanie <128801647+Allin1billingandcredentialing@users.noreply.github.com>
2025-04-19 04:40:10 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Dashboard from './pages/Dashboard';

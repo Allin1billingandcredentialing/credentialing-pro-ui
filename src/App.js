525399c7ea8393733baafda9fd8116b3e2af64ca
Stephanie <128801647+Allin1billingandcredentialing@users.noreply.github.com>
2025-04-19 05:05:54 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,34 +1,23 @@
 import React from 'react';
-import { HashRouter as Router } from 'react-router-dom';
-import Login from './pages/Login';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Register from './pages/Register';
+import Login from './pages/Login';
 import Dashboard from './pages/Dashboard';
-import ProtectedRoute from './components/ProtectedRoute';
-import { AuthProvider } from './context/AuthContext';
 
 function App() {
   return (
-    <AuthProvider>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route
-            path="/dashboard"
-            element={
-              <ProtectedRoute>
-                <Dashboard />
-              </ProtectedRoute>
-            }
-          />
-        </Routes>
-      </Router>
-    </AuthProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
-
 
 
 
